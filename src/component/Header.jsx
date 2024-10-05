96e93711a6909fb322188a9397db9ca938806111
Alka Sah <alkasah485@gmail.com>
2024-10-05 15:25:35 -0700

    modification in Header comp

---
+++ src/component/Header.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <a href="#" className='logo'>A <span>LK</span>A</a>
+      <nav className='navbar'>
+        <a href="#" className='active'>Portfolio</a>
+        <a href="#">Projects</a>
+        <a href="#">Education</a>
+        <a href="#">About Me</a>
+      </nav>
+      <a href="#" className='contact'>Contact Me</a>
+    </div>
+  )
+}
+
+export default Header
+
