c07630bc155951fd2d0be44d4f1f6a49835fef0d
Alka Sah <alkasah485@gmail.com>
2024-10-18 13:26:32 -0700

    modification saved

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -9,9 +9,7 @@ const Header = () => {
       <a href="/" className='logo'>A<span>LK</span>A</a>
       <nav className='navbar'>
         <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Portfolio</Link>
-        {/* <a href="/projects">Projects</a> */}
         <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>Projects</Link>
-        <a href="#">Education</a>
         <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Me</Link>
       </nav>
       <Link to="/contact" className='contact'>Contact Me</Link>
