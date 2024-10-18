73d111fc9990083d151da5e2c0ebfee7a0df1053
Alka Sah <alkasah485@gmail.com>
2024-10-18 13:10:40 -0700

    modification saved

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useLocation } from 'react-router-dom'
 
 const Header = () => {
+  const location = useLocation();
   return (
     <div className='header'>
       <a href="/" className='logo'>A<span>LK</span>A</a>
       <nav className='navbar'>
-        <Link to="/" className='active'>Portfolio</Link>
-        <a href="/projects">Projects</a>
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Portfolio</Link>
+        {/* <a href="/projects">Projects</a> */}
+        <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>Projects</Link>
         <a href="#">Education</a>
-        <Link to="/about">About Me</Link>
+        <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Me</Link>
       </nav>
       <Link to="/contact" className='contact'>Contact Me</Link>
     </div>
