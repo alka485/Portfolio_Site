1c80d0bbc1cff4338a2eb0cd5dec601a70feb7c1
Alka Sah <alkasah485@gmail.com>
2024-10-17 23:44:21 -0700

    modifications saved

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <a href="/" className='logo'>A<span>LK</span>A</a>
       <nav className='navbar'>
         <Link to="/" className='active'>Portfolio</Link>
-        <a href="#">Projects</a>
+        <a href="/projects">Projects</a>
         <a href="#">Education</a>
         <Link to="/about">About Me</Link>
       </nav>
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -2,9 +2,22 @@
 import React from 'react'
 import projects from './Projects.json'
 
-const Projects = (props) => {
+const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects-content'>
+        {projects.map((project) => (
+            <div key={project.id} className='projects'>
+                <h3>{project.name}</h3>
+                <img src={project.image} alt={project.name} className='project-image' />
+                <p>{project.description}</p>
+                <div className="project-link">
+                    <a href={project.github} target='blank'>GitHub</a>
+                    <a href={project.deploy} target='blank'>Live Demo</a>
+                </div>
+                <p><strong>Skills</strong>{project.skills}</p>
+            </div>
+        ))}
+    </div>
   )
 }
 
